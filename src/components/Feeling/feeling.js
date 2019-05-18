7e5c6b56a1b35e20edc946b7692e373c1d6cc8db
Austin Burgard <awburgard@gmail.com>
2019-05-18 13:14:29 -0500

    adds input field and change event to feeling component

--- src/components/Feeling/feeling.js
+++ src/components/Feeling/feeling.js
@@ -5,10 +5,26 @@ import mapReduxStateToProps from '../../Modules/mapReduxStateToProps'
 class Feeling extends Component{
     constructor(props){
         super(props)
+        this.state = {
+            feeling : ''
+        }
+    }
+
+    changeFeeling = (event) => {
+        const inputValue = event.target.value;
+        this.setState ({
+            feeling: inputValue,
+        });
+
     }
     render(){
         return(
-            <div>Hello</div>
+            <div>
+                <input
+                type="number"
+                placeholder="How are you feeling?"
+                onChange={this.changeFeeling}/>
+            </div>
         )
     }
 
