dfc1e667b209bf95e54438f6f22ae9a0ae5d508a
Austin Burgard <awburgard@gmail.com>
2019-05-18 14:10:42 -0500

    adds get and post routes to server

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -1,10 +1,32 @@
 const express = require('./node_modules/express');
 const router = express.Router();
+const pool = require('../modules/pool');
 
+router.get('/', (req, res)=>{
+    const feedbackQuery = `SELECT * FROM "feedback" ORDER BY "feeling" ASC;`;
+    pool.query(feedbackQuery)
+    .then((response)=>{
+        res.send(response.rows)
+    })
+    .catch((err)=>{
+        console.log('Error getting data from the database', err);
+        res.sendStatus(500);
+    })
+})
 
-router.get();
-router.post();
-router.put();
-router.delete();
+router.post('/', (req, res)=>{
+    const fbObject = req.body;
+    const feedbackQuery = `INSERT INTO "feedback" ("feeling",  "understanding", "support", "comments")
+                            VALUES ($1, $2, $3, $4);`;
+    pool.query(feedbackQuery, [fbObject.feeling, fbObject.understanding, fbObject.support, fbObject.comments])
+    .then((res) => {
+        res.sendStatus(201);
+    })
+    .catch((err) => {
+        console.log('Error saving to DB:', err)
+        res.sendStatus(500)
+    });
+});
+
 
 module.exports = router;
