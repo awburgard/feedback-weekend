d980867a92bc92987099d0cab6dde7cb95af6f2f
Austin Burgard <awburgard@gmail.com>
2019-05-19 17:34:54 -0500

    adds styling to admin

--- src/components/Admin/admin.js
+++ src/components/Admin/admin.js
@@ -7,19 +7,22 @@ class Admin extends Component {
         this.props.history.push('/')
     }
     render() {
-        const feedbackArray = this.props.reduxState.finalReducer.map((feedback, index)=>{
-            return(
-                <div key={index}>
-                    <p>Feeling: {feedback.feeling}</p>
-                    <p>Understanding: {feedback.understanding}</p>
-                    <p>Support: {feedback.support}</p>
-                    <p>Comments: {feedback.comments}</p>
+        const feedbackArray = this.props.reduxState.finalReducer.map((feedback, index) => {
+            return (
+                <div key={index} className="box">
+                    <div className="content">
+                        <p>Feeling: {feedback.feeling}</p>
+                        <p>Understanding: {feedback.understanding}</p>
+                        <p>Support: {feedback.support}</p>
+                        <p>Comments: {feedback.comments}</p>
+                    </div>
                 </div>
             )
         })
 
-            return (
+        return (
             <div>
+                <h2 className="title is-4">Past Feedback</h2>
                 {feedbackArray}
                 <button className="button is-info" onClick={this.goHome}>Home</button>
             </div>
