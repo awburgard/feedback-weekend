6621c65888c0825142933620a868c340090112df
Austin Burgard <awburgard@gmail.com>
2019-07-02 17:57:07 -0500

    removes back button

--- src/components/Review/review.js
+++ src/components/Review/review.js
@@ -23,32 +23,27 @@ class Review extends Component {
                 this.props.history.push('/success');
             })
     }
-    goBack = (event) => {
-        this.props.history.push('/comments')
-    }
 
     render() {
         let disableButton = true;
 
-        if( this.props.reduxState.feedbackReducer.feeling &&
+        if (this.props.reduxState.feedbackReducer.feeling &&
             this.props.reduxState.feedbackReducer.understanding &&
             this.props.reduxState.feedbackReducer.support &&
             this.props.reduxState.feedbackReducer.comments) {
             disableButton = false;
         }
         return (
-            <div className="box">
-                <div className="content">
-                    <h3>Review Your Feedback</h3>
-                    <p>Feeling: {this.props.reduxState.feedbackReducer.feeling}</p>
-                    <p>Understanding: {this.props.reduxState.feedbackReducer.understanding}</p>
-                    <p>Support: {this.props.reduxState.feedbackReducer.support}</p>
-                    <p>Comments: {this.props.reduxState.feedbackReducer.comments}</p>
+                <div className="box">
+                    <div className="content">
+                        <h3>Review Your Feedback</h3>
+                        <p>Feeling: {this.props.reduxState.feedbackReducer.feeling}</p>
+                        <p>Understanding: {this.props.reduxState.feedbackReducer.understanding}</p>
+                        <p>Support: {this.props.reduxState.feedbackReducer.support}</p>
+                        <p>Comments: {this.props.reduxState.feedbackReducer.comments}</p>
+                    </div>
+                    <button disabled={disableButton} className="button is-rounded is-info is-inverted" onClick={this.addReviewToDatabase}>Finish</button>
                 </div>
-                <button className="button is-rounded is-info is-inverted" onClick={this.goBack}>Back</button>
-                <button disabled={disableButton} className="button is-rounded is-info is-inverted" onClick={this.addReviewToDatabase}>Finish</button>
-            </div>
-
         )
     }
 }
